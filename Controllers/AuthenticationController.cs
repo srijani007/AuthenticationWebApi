1346d407f7504b75ba920995c509199add1eb0b7
cogdotnet1354 <cogdotnet1354@CTSDOTNET672>
2022-08-16 21:47:33 +0530

    Add project files.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,64 @@
+﻿using AuthenticationWebApi.Services;
+using AuthorApi.Services;
+using CommonSpace.DatabaseEntity;
+using CommonSpace.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : Controller
+    {
+        private readonly IAuthenticationServices _authenticationServices;
+        private readonly IConfiguration _configuration;
+        public AuthenticationController(IConfiguration configuration, IAuthenticationServices authenticationServices)
+        {
+            this._configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _authenticationServices = authenticationServices;//?? throw new ArgumentNullException(nameof(authenticationServices));
+        }
+
+        [HttpPost]
+        public ActionResult<string> Validate(Creds usercreds)
+        {
+            try
+            {
+                if (_authenticationServices.validateuser(usercreds))
+                {
+                    var Token = _authenticationServices.BuildToken(_configuration["Jwt:Key"],
+                                                    _configuration["Jwt:Issuer"],
+                                                    new[]
+                                                    {
+                                             //     _configuration["Jwt:AudienceGateway"],
+                                                  _configuration["Jwt:Audience"],
+                                                        //    _configuration["Jwt:Audiencebook"]
+                                                    },
+                                                    usercreds.UserName
+                                                    );
+                    return Ok(new
+                    {
+                        Token = Token,
+                        IsAuthenticated = true,
+                    });
+                }
+                return Ok(new
+                {
+                    Token = string.Empty,
+                    IsAuthenticated = false
+                });
+
+            }
+
+            catch (Exception ex)
+            {
+                return Unauthorized();
+            }
+
+        }
+
+    }
+}
---
+++ Services/AuthenticationServices.cs
@@ -0,0 +1,52 @@
+﻿using CommonSpace.DatabaseEntity;
+using CommonSpace.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationWebApi.Services
+{
+    public class AuthenticationServices : IAuthenticationServices
+    {
+        public LibraryContext libDbContext { get; set; }
+
+        public AuthenticationServices(LibraryContext libdbContext)
+        {
+            libDbContext = libdbContext;
+        }
+
+        public bool validateuser(Creds usersdetails)
+        {
+            var userObj = libDbContext.DigitalBooksUsers.Where(u => u.UserName == usersdetails.UserName && u.UserPass == usersdetails.UserPass).Count();
+            if (userObj > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private TimeSpan ExpiryDuration = new TimeSpan(20, 30, 0);
+        public string BuildToken(string key, string issuer, IEnumerable<string> audience, string userName)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.UniqueName, userName),
+            new Claim("RoleId","Author")
+
+        };
+
+            claims.AddRange(audience.Select(aud => new Claim(JwtRegisteredClaimNames.Aud, aud)));
+
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256Signature);
+            var tokendescriptor = new JwtSecurityToken(issuer, issuer, claims,
+                expires: DateTime.Now.Add(ExpiryDuration), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(tokendescriptor);
+        }
+
+    }
+}
